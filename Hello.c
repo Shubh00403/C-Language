d998d87ed94d92622ea87e6f14d931a96139e3c6
Shubh00403 <rshubhamkumar004@gmail.com>
2025-05-07 15:00:04 +0530

    third Commit

--- Hello.c
+++ Hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World\n");
+    int age = 20;
+    float pi = 3.14;
+    char star = '*';
+    printf("%d\n", age);
+    printf("%f\n", pi);
+    printf("%c\n", star);
+    
     return 0;
 }
